710260aac54aa4c776dc1e2536e72337398fdad8
XU PU <pxuaf@connect.ust.hk>
2022-01-17 00:26:35 +0800

    encoder printer worked

--- pyddh/encoder_printer.py
+++ pyddh/encoder_printer.py
@@ -3,7 +3,9 @@ from .ddh import DDGripper
 
 if __name__ == '__main__':
     gripper = DDGripper('ddh_default')
-    print('Encoder Readings:')
+    print('------------------------------')
+    print('Press ENTER to take a snapshot')
+    print('------------------------------')
     while True:
         time.sleep(1./100.)
         print('R0: {0:.15f}, R1: {1:.15f}, L0: {2:.15f}, L1: {3:.15f}'.format(gripper.R0.encoder, gripper.R1.encoder, gripper.L0.encoder, gripper.L1.encoder), end="\r")
