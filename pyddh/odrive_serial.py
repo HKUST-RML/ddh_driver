ab8c01c8446335551911102d25762e8f731cf185
XU PU <pxuaf@connect.ust.hk>
2022-01-16 23:34:18 +0800

    add odrive_serial module

---
+++ pyddh/odrive_serial.py
@@ -0,0 +1,8 @@
+#!/usr/env python
+
+import odrive
+
+if __name__ == '__main__':
+    odv = odrive.find_any()
+    print('ODrive Found')
+    print('Serial Number: %s' % format(odv.serial_number, 'X'))
