8abeda109247a8edb51cdd60473014dfc03f5458
XU PU <pxuaf@connect.ust.hk>
2022-02-03 02:30:54 +0800

    add yaml loading utils

--- ddh_driver/utils.py
+++ ddh_driver/utils.py
@@ -14,6 +14,18 @@ def load_ddh_config(name):
     return config
 
 
+def load_yaml(local_path):
+    config_path = os.path.join(os.path.dirname(__file__), '..', local_path)
+    print('Loading YAML at %s' % os.path.abspath(config_path))
+    config = {}
+    with open(config_path, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+
 def get_abs_path(local_path):
     p = os.path.join(os.path.dirname(__file__), '..', local_path)
     return os.path.abspath(p)
